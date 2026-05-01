b05662b4b4bff581f6ce3d8d239ff9f7c670eaba
nicolekl7 <nicole.landmesser@aol.com>
2026-05-01 10:40:11 -0400

    Update OurStoryV2.tsx

--- src/pages/OurStoryV2.tsx
+++ src/pages/OurStoryV2.tsx
@@ -111,7 +111,7 @@ const stops: RoadmapStopData[] = [
       { src: ct2, nudgeX: 60, rotate: -10 },
       { src: ct3, rotate: -10},
       { src: ct4, nudgeX: 40, rotate: 2 },
-      { src: ct5, nudgeX: -40, rotate: -10 },
+      { src: ct5, nudgeX: -40, rotate: 1 },
     ],
   },
   {
