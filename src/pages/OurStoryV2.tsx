f2eb24081b8b46a33479384d58429182db3fa16d
nicolekl7 <nicole.landmesser@aol.com>
2026-04-30 23:16:27 -0400

    Adjust rotation for photo ct3 in OurStoryV2

--- src/pages/OurStoryV2.tsx
+++ src/pages/OurStoryV2.tsx
@@ -109,7 +109,7 @@ const stops: RoadmapStopData[] = [
     photos: [
       { src: ct1 },
       { src: ct2, nudgeX: 60, rotate: -60 },
-      { src: ct3 },
+      { src: ct3, rotate: 80},
       { src: ct4, nudgeX: 40, rotate: -30 },
       { src: ct5, nudgeX: -40 },
     ],
